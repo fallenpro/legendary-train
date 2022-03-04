84a49bcdee39472eac66818d397d384bd1131e17
fheahdythdr <100938045+fheahdythdr@users.noreply.github.com>
2022-03-04 16:57:42 +0200

    Add files via upload

---
+++ BrookhavenRainbowMan.lua
@@ -0,0 +1,157 @@
+--[[ 
+
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣠⣤⣤⣴⣦⣤⣤⣄⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⢀⣤⣾⣿⣿⣿⣿⠿⠿⠿⠿⣿⣿⣿⣿⣶⣤⡀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⣠⣾⣿⣿⡿⠛⠉⠀⠀⠀⠀⠀⠀⠀⠀⠉⠛⢿⣿⣿⣶⡀⠀⠀⠀⠀
+⠀⠀⠀⣴⣿⣿⠟⠁⠀⠀⠀⣶⣶⣶⣶⡆⠀⠀⠀⠀⠀⠀⠈⠻⣿⣿⣦⠀⠀⠀
+⠀⠀⣼⣿⣿⠋⠀⠀⠀⠀⠀⠛⠛⢻⣿⣿⡀⠀⠀⠀⠀⠀⠀⠀⠙⣿⣿⣧⠀⠀
+⠀⢸⣿⣿⠃⠀⠀⠀⠀⠀⠀⠀⠀⢀⣿⣿⣷⠀⠀⠀⠀⠀⠀⠀⠀⠸⣿⣿⡇⠀
+⠀⣿⣿⡿⠀⠀⠀⠀⠀⠀⠀⠀⢀⣾⣿⣿⣿⣇⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⠀
+⠀⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⢠⣿⣿⡟⢹⣿⣿⡆⠀⠀⠀⠀⠀⠀⠀⣹⣿⣿⠀
+⠀⣿⣿⣷⠀⠀⠀⠀⠀⠀⣰⣿⣿⠏⠀⠀⢻⣿⣿⡄⠀⠀⠀⠀⠀⠀⣿⣿⡿⠀
+⠀⢸⣿⣿⡆⠀⠀⠀⠀⣴⣿⡿⠃⠀⠀⠀⠈⢿⣿⣷⣤⣤⡆⠀⠀⣰⣿⣿⠇⠀
+⠀⠀⢻⣿⣿⣄⠀⠀⠾⠿⠿⠁⠀⠀⠀⠀⠀⠘⣿⣿⡿⠿⠛⠀⣰⣿⣿⡟⠀⠀
+⠀⠀⠀⠻⣿⣿⣧⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣾⣿⣿⠏⠀⠀⠀
+⠀⠀⠀⠀⠈⠻⣿⣿⣷⣤⣄⡀⠀⠀⠀⠀⠀⠀⢀⣠⣴⣾⣿⣿⠟⠁⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠈⠛⠿⣿⣿⣿⣿⣿⣶⣶⣿⣿⣿⣿⣿⠿⠋⠁⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠉⠛⠛⠛⠛⠛⠛⠉⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+BROOKHAVEN :: RAINBOWMAN
+
+        DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE 
+                    Version 2, December 2004 
+
+ Copyright (C) 2004 Sam Hocevar <sam@hocevar.net> 
+
+ Everyone is permitted to copy and distribute verbatim or modified 
+ copies of this license document, and changing it is allowed as long 
+ as the name is changed. 
+
+            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE 
+   TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION 
+
+  0. You just DO WHAT THE FUCK YOU WANT TO.
+  
+
+]]
+
+local rTime = 0.1
+local toggle = true
+
+while toggle do
+    game:GetService("ReplicatedStorage").RemoteEvents.U14pdateA14vatar:FireServer("skintone", "Pastel brown") -- 001
+    wait(rTime)
+    game:GetService("ReplicatedStorage").RemoteEvents.U14pdateA14vatar:FireServer("skintone", "Light orange") -- 002
+    wait(rTime)
+    game:GetService("ReplicatedStorage").RemoteEvents.U14pdateA14vatar:FireServer("skintone", "Dark nougat") -- 003
+    wait(rTime)
+    game:GetService("ReplicatedStorage").RemoteEvents.U14pdateA14vatar:FireServer("skintone", "Brown") -- 004
+    wait(rTime)
+    game:GetService("ReplicatedStorage").RemoteEvents.U14pdateA14vatar:FireServer("skintone", "Pine Cone") -- 005
+    wait(rTime)
+    game:GetService("ReplicatedStorage").RemoteEvents.U14pdateA14vatar:FireServer("skintone", "Dirt brown") -- 006
+    wait(rTime)
+    game:GetService("ReplicatedStorage").RemoteEvents.U14pdateA14vatar:FireServer("skintone", "Really black") -- 007
+    wait(rTime)
+    game:GetService("ReplicatedStorage").RemoteEvents.U14pdateA14vatar:FireServer("skintone", "Black") -- 008
+    wait(rTime)
+    game:GetService("ReplicatedStorage").RemoteEvents.U14pdateA14vatar:FireServer("skintone", "Smoky grey") -- 009
+    wait(rTime)
+    game:GetService("ReplicatedStorage").RemoteEvents.U14pdateA14vatar:FireServer("skintone", "Dark stone grey") -- 010
+    wait(rTime)
+    game:GetService("ReplicatedStorage").RemoteEvents.U14pdateA14vatar:FireServer("skintone", "Medium stone grey") -- 011
+    wait(rTime)
+    game:GetService("ReplicatedStorage").RemoteEvents.U14pdateA14vatar:FireServer("skintone", "Fossil") -- 012
+    wait(rTime)
+    game:GetService("ReplicatedStorage").RemoteEvents.U14pdateA14vatar:FireServer("skintone", "Flint") -- 013
+    wait(rTime)
+    game:GetService("ReplicatedStorage").RemoteEvents.U14pdateA14vatar:FireServer("skintone", "Cloudy grey") -- 014
+    wait(rTime)
+    game:GetService("ReplicatedStorage").RemoteEvents.U14pdateA14vatar:FireServer("skintone", "Mid gray") -- 015
+    wait(rTime)
+    game:GetService("ReplicatedStorage").RemoteEvents.U14pdateA14vatar:FireServer("skintone", "Insitutional white") -- 016
+    wait(rTime)
+    game:GetService("ReplicatedStorage").RemoteEvents.U14pdateA14vatar:FireServer("skintone", "Mulberry") -- 017
+    wait(rTime)
+    game:GetService("ReplicatedStorage").RemoteEvents.U14pdateA14vatar:FireServer("skintone", "Eggplant") -- 018
+    wait(rTime)
+    game:GetService("ReplicatedStorage").RemoteEvents.U14pdateA14vatar:FireServer("skintone", "Bright violet") -- 019
+    wait(rTime)
+    game:GetService("ReplicatedStorage").RemoteEvents.U14pdateA14vatar:FireServer("skintone", "Magenta") -- 020
+    wait(rTime)
+    game:GetService("ReplicatedStorage").RemoteEvents.U14pdateA14vatar:FireServer("skintone", "Royal purple") -- 021
+    wait(rTime)
+    game:GetService("ReplicatedStorage").RemoteEvents.U14pdateA14vatar:FireServer("skintone", "Navy blue") -- 022
+    wait(rTime)
+    game:GetService("ReplicatedStorage").RemoteEvents.U14pdateA14vatar:FireServer("skintone", "Dark blue") -- 023
+    wait(rTime)
+    game:GetService("ReplicatedStorage").RemoteEvents.U14pdateA14vatar:FireServer("skintone", "Really blue") -- 024
+    wait(rTime)
+    game:GetService("ReplicatedStorage").RemoteEvents.U14pdateA14vatar:FireServer("skintone", "Bright blue") -- 025
+    wait(rTime)
+    game:GetService("ReplicatedStorage").RemoteEvents.U14pdateA14vatar:FireServer("skintone", "Cyan") -- 026
+    wait(rTime)
+    game:GetService("ReplicatedStorage").RemoteEvents.U14pdateA14vatar:FireServer("skintone", "Pastel light blue") -- 027
+    wait(rTime)
+    game:GetService("ReplicatedStorage").RemoteEvents.U14pdateA14vatar:FireServer("skintone", "Toothpaste") -- 028
+    wait(rTime)
+    game:GetService("ReplicatedStorage").RemoteEvents.U14pdateA14vatar:FireServer("skintone", "Slime green") -- 029
+    wait(rTime)
+    game:GetService("ReplicatedStorage").RemoteEvents.U14pdateA14vatar:FireServer("skintone", "Earth green") -- 030
+    wait(rTime)
+    game:GetService("ReplicatedStorage").RemoteEvents.U14pdateA14vatar:FireServer("skintone", "Paresly green") -- 031
+    wait(rTime)
+    game:GetService("ReplicatedStorage").RemoteEvents.U14pdateA14vatar:FireServer("skintone", "Forest green") -- 032
+    wait(rTime)
+    game:GetService("ReplicatedStorage").RemoteEvents.U14pdateA14vatar:FireServer("skintone", "Grime") -- 033
+    wait(rTime)
+    game:GetService("ReplicatedStorage").RemoteEvents.U14pdateA14vatar:FireServer("skintone", "Lime green") -- 034
+    wait(rTime)
+    game:GetService("ReplicatedStorage").RemoteEvents.U14pdateA14vatar:FireServer("skintone", "Artichoke") -- 035
+    wait(rTime)
+    game:GetService("ReplicatedStorage").RemoteEvents.U14pdateA14vatar:FireServer("skintone", "Mint") -- 036
+    wait(rTime)
+    game:GetService("ReplicatedStorage").RemoteEvents.U14pdateA14vatar:FireServer("skintone", "Br. yellowish green") -- 037
+    wait(rTime)
+    game:GetService("ReplicatedStorage").RemoteEvents.U14pdateA14vatar:FireServer("skintone", "New Yeller") -- 038
+    wait(rTime)
+    game:GetService("ReplicatedStorage").RemoteEvents.U14pdateA14vatar:FireServer("skintone", "Olive") -- 039
+    wait(rTime)
+    game:GetService("ReplicatedStorage").RemoteEvents.U14pdateA14vatar:FireServer("skintone", "Deep orange") -- 040
+    wait(rTime)
+    game:GetService("ReplicatedStorage").RemoteEvents.U14pdateA14vatar:FireServer("skintone", "Br. yellowish orange") -- 041
+    wait(rTime)
+    game:GetService("ReplicatedStorage").RemoteEvents.U14pdateA14vatar:FireServer("skintone", "Fawn brown") -- 042
+    wait(rTime)
+    game:GetService("ReplicatedStorage").RemoteEvents.U14pdateA14vatar:FireServer("skintone", "Burlap") -- 043
+    wait(rTime)
+    game:GetService("ReplicatedStorage").RemoteEvents.U14pdateA14vatar:FireServer("skintone", "Brick yellow") -- 044
+    wait(rTime)
+    game:GetService("ReplicatedStorage").RemoteEvents.U14pdateA14vatar:FireServer("skintone", "Beige") -- 045
+    wait(rTime)
+    game:GetService("ReplicatedStorage").RemoteEvents.U14pdateA14vatar:FireServer("skintone", "CGA brown") -- 046
+    wait(rTime)
+    game:GetService("ReplicatedStorage").RemoteEvents.U14pdateA14vatar:FireServer("skintone", "Rust") -- 047
+    wait(rTime)
+    game:GetService("ReplicatedStorage").RemoteEvents.U14pdateA14vatar:FireServer("skintone", "Cocoa") -- 048
+    wait(rTime)
+    game:GetService("ReplicatedStorage").RemoteEvents.U14pdateA14vatar:FireServer("skintone", "Maroon") -- 049
+    wait(rTime)
+    game:GetService("ReplicatedStorage").RemoteEvents.U14pdateA14vatar:FireServer("skintone", "Burgundy") -- 050
+    wait(rTime)
+    game:GetService("ReplicatedStorage").RemoteEvents.U14pdateA14vatar:FireServer("skintone", "Tawny") -- 051
+    wait(rTime)
+    game:GetService("ReplicatedStorage").RemoteEvents.U14pdateA14vatar:FireServer("skintone", "Really red") -- 052
+    wait(rTime)
+    game:GetService("ReplicatedStorage").RemoteEvents.U14pdateA14vatar:FireServer("skintone", "Hot pink") -- 053
+    wait(rTime)
+    game:GetService("ReplicatedStorage").RemoteEvents.U14pdateA14vatar:FireServer("skintone", "Pink") -- 054
+    wait(rTime)
+    game:GetService("ReplicatedStorage").RemoteEvents.U14pdateA14vatar:FireServer("skintone", "Carnation pink") -- 055
+    wait(rTime)
+    game:GetService("ReplicatedStorage").RemoteEvents.U14pdateA14vatar:FireServer("skintone", "Sunrise") -- 056
+    wait(rTime)
+    game:GetService("ReplicatedStorage").RemoteEvents.U14pdateA14vatar:FireServer("skintone", "Salmon") -- 057
+    wait(rTime)
+    game:GetService("ReplicatedStorage").RemoteEvents.U14pdateA14vatar:FireServer("skintone", "Light reddish violet") -- 058
+    wait(rTime)
+end
