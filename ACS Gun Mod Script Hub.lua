07dc1a81822e1cbfb35a41b08476c14402050e53
Tanki <100938045+fheahdythdr@users.noreply.github.com>
2022-03-04 23:45:04 +0200

    Update ACS Gun Mod Script Hub.lua

--- ACS Gun Mod Script Hub.lua
+++ ACS Gun Mod Script Hub.lua
@@ -1,1 +1,352 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/fheahdythdr/vtgbdfnbyhf/main/Script%20Hub.lua"))()
+local library = loadstring(game:HttpGet(('https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/wall%20v3')))()
+local w = library:CreateWindow("Tanki's Script Hub")
+local b = w:CreateFolder("ACS Gun System Mods",{
+    TextSize = 25;
+    TextColor = Color3.fromRGB(255,255,255);
+    BgColor = Color3.fromRGB(0,0,0);
+})
+
+b:Button("Cold's Gun Mods", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/fheahdythdr/miniature-tribble/main/ACS%20Cold's%20Modifications.lua"))()
+end)
+
+b:Button("Explosive Bullets", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/fheahdythdr/miniature-tribble/main/ACS%20Giant%20Explosions.lua"))()
+end)
+
+b:Button("HE Bullets", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/fheahdythdr/miniature-tribble/main/Nuclear.lua"))()
+end)
+
+b:Button("Bye, gun safety!", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/fheahdythdr/miniature-tribble/main/ACS%20Tsar%20Bomba%20Bullets.lua"))()
+end)
+
+local b = w:CreateFolder("VS",{
+    TextSize = 25;
+    TextColor = Color3.fromRGB(255,255,255);
+    BgColor = Color3.fromRGB(0,0,0);
+})
+
+b:Button("Vehicle Simulator", function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/WetCheezit/Releases/main/VehicleSimulator/Source.lua"))()
+end)
+
+local b = w:CreateFolder("Clicking Simulator",{
+    TextSize = 25;
+    TextColor = Color3.fromRGB(255,255,255);
+    BgColor = Color3.fromRGB(0,0,0);
+})
+
+b:Button("CS", function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/LaDamage/releases/main/Clicker-Simulator.lua"))()
+end)
+
+local b = w:CreateFolder("Jailbird")
+
+b:Button("Asgar's GUI", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/1iseo/rbx/main/jailbird/jailbird.lua"))()
+end)
+
+b:Button("Gun Mod", function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/fheahdythdr/miniature-tribble/main/jbgm.lua"))()
+end)
+
+b:Button("Gun Mod v2", function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/fheahdythdr/miniature-tribble/main/jbgm%20v2.lua"))()
+end)
+ 
+local b = w:CreateFolder("BIG Paintball",{
+    TextSize = 25;
+    TextColor= Color3.fromRGB(255,255,255);
+    BgColor = Color3.fromRGB(0,0,0);
+})
+
+b:Button("Unlock All", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/fheahdythdr/miniature-tribble/main/big%20paintball%20unlock%20all.lua"))()
+end)
+
+b:Button("omaewa mou", function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/fheahdythdr/miniature-tribble/main/teleports%20behind%20you.lua"))()
+end)
+
+b:Button("KillAll", function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/The3Bakers4565/Spicy-Bagel/main/Other/Big_Paintball/Kill_All.lua"))()
+end)
+ 
+local b = w:CreateFolder("Isle",{
+    TextSize = 25;
+    TextColor= Color3.fromRGB(255,255,255);
+    BgColor = Color3.fromRGB(0,0,0);
+})
+ 
+b:Button("ESP", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/fheahdythdr/miniature-tribble/main/superior%20isle%20esp.lua"))()
+end)
+ 
+local b = w:CreateFolder("State of Anarchy",{
+    TextSize = 25;
+    TextColor= Color3.fromRGB(255,255,255);
+    BgColor = Color3.fromRGB(0,0,0);
+})
+ 
+b:Button("SOA GUI", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/fheahdythdr/miniature-tribble/main/soa.lua"))()
+end)
+ 
+local b = w:CreateFolder("SCP RP",{
+    TextSize = 25;
+    TextColor= Color3.fromRGB(255,255,255);
+    BgColor = Color3.fromRGB(0,0,0);
+})
+ 
+b:Button("SCP RP", function()
+    loadstring(game:HttpGet("https://anomaly.cool/scripts/scp-rp.lua"))()
+end)
+ 
+local b = w:CreateFolder("Recoil Zombies",{
+    TextSize = 25;
+    TextColor= Color3.fromRGB(255,255,255);
+    BgColor = Color3.fromRGB(0,0,0);
+})
+ 
+b:Button("Gun Giver", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/fheahdythdr/miniature-tribble/main/Recoil_zombie_script.lua"))()
+end)
+ 
+local b = w:CreateFolder("Mostly Universal",{
+    TextSize = 25;
+    TextColor= Color3.fromRGB(255,255,255);
+    BgColor = Color3.fromRGB(0,0,0);
+})
+ 
+b:Button("VG Hub", function()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/1201for/V.G-Hub/main/V.Ghub'))()
+end)
+ 
+local b = w:CreateFolder("Memes",{
+    TextSize = 25;
+    TextColor = Color3.fromRGB(255,255,255);
+    BgColor = Color3.fromRGB(0,0,0)
+    })
+ 
+b:Button("Ukraine", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/fheahdythdr/miniature-tribble/main/Ukraine.lua"))()
+end)
+
+b:Button("Invis Car", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/fheahdythdr/miniature-tribble/main/car.lua"))()
+end)
+ 
+local b = w:CreateFolder("Arsenal",{
+    TextSize = 25;
+    TextColor = Color3.fromRGB(255,255,255);
+    BgColor = Color3.fromRGB(0,0,0);
+})
+ 
+b:Button("Gaming Chair", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/fheahdythdr/miniature-tribble/main/Aresenal_Gaming_Chair.lua"))()
+end)
+ 
+local b = w:CreateFolder("Modded Scripts",{
+    TextSize = 25;
+    TextColor = Color3.fromRGB(255,255,255);
+    BgColor = Color3.fromRGB(0,0,0);
+})
+ 
+b:Button("Aimhot", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Babyhamsta/RBLX_Scripts/main/Frontlines/Modded_Aimhot.lua", true))()
+end)
+ 
+local b = w:CreateFolder("Cafe Destroyers",{
+    TextSize = 25;
+    TextColor = Color3.fromRGB(255,255,255);
+    BgColor = Color3.fromRGB(0,0,0);
+})
+ 
+b:Button("op_killer", function()
+    loadstring(game:HttpGet("https://pastebin.com/raw/v8PX741z"))()
+end)
+ 
+local b = w:CreateFolder("Frontlines",{
+    TextSize = 25;
+    TextColor = Color3.fromRGB(255,255,255);
+    BgColor = Color3.fromRGB(0,0,0);
+})
+ 
+b:Button("wally", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/wally-rblx/roblox-scripts/main/frontlines.lua"))()
+end)
+ 
+local b = w:CreateFolder("Destruction Sim",{
+    TextSize = 25;
+    TextColor = Color3.fromRGB(255,255,255);
+    BgColor = Color3.fromRGB(0,0,0);
+})
+ 
+b:Button("Destruction Sim", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/FadeRemix/RND-Games/main/DestructionSim.lua"))()
+end)
+ 
+local b = w:CreateFolder("SCP 3008",{
+    TextSize = 25;
+    TextColor = Color3.fromRGB(255,255,255);
+    BgColor = Color3.fromRGB(0,0,0);
+})
+ 
+b:Button("Anticheat Bypass", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/fheahdythdr/miniature-tribble/main/SCP%203008%20anticheat%20bypass.lua"))()
+end)
+
+local b = w:CreateFolder("Legends of Speed",{
+    TextSize = 25;
+    TextColor = Color3.fromRGB(255,255,255);
+    BgColor = Color3.fromRGB(0,0,0);
+})
+
+b:Button("LOS", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Coldlolololir/bfdzhuiasbghjdlbgjhlas/main/LOS.lua"))()
+end)
+
+local b = w:CreateWindow("Utilities")
+
+local b = w:CreateFolder("Main",{
+    TextSize = 25;
+    TextColor = Color3.fromRGB(255,255,255);
+    BgColor = Color3.fromRGB(0,0,0);
+})
+
+b:Button("Re-execute UI", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/fheahdythdr/miniature-tribble/main/ACS%20Gun%20Mod%20Script%20Hub.lua"))()
+end)
+
+b:Button("Server Browser", function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/Lynk-Softworks/Valerie/main/serverBrowser.lua", true))()
+end)
+
+b:Button("Custom Dex V4", function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/loglizzy/dexV4/main/main.lua"))()
+end)
+
+b:Button("Custom Dex V3", function()
+        loadstring(game:HttpGet("https://pastebin.com/raw/YREfugjX"))()
+end)
+
+b:Button("AimHot", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Herrtt/AimHot-v8/master/Main.lua", true))()
+end)
+ 
+b:Button("Silent Aim", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Averiias/Universal-SilentAim/main/main.lua"))()
+end)
+ 
+b:Button("Infinite Store", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Infinite-Store/Infinite-Store/main/main.lua"))()
+end)
+ 
+b:Button("WalkOnWalls", function()
+    loadstring(game:HttpGet("https://pastebin.com/raw/zXk4Rq2r"))()
+end)
+ 
+b:Button("LagSwitch", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/fheahdythdr/miniature-tribble/main/LagSwitch.lua"))()
+end)
+ 
+b:Button("BackdoorChecker", function()
+    loadstring(game:HttpGet(('https://raw.githubusercontent.com/iK4oS/backdoor.exe/master/source.lua'),true))()
+end)
+ 
+b:Button("CMD-X", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/CMD-X/CMD-X/master/Source", true))()
+end)
+
+local w = library:CreateWindow("Page 2")
+
+local b = w:CreateFolder("Anomic",{
+    TextSize = 25;
+    TextColor = Color3.fromRGB(255,255,255);
+    BgColor = Color3.fromRGB(0,0,0);
+})
+ 
+b:Button("Anomic Revamp", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Shariiii/Anomic-Revamp-aka-Anomic-v2-/main/OP%20OP%20Gun%20Script%20V2"))()
+end)
+
+b:Button("Anomic", function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/Shariiii/Anomic-Stuff/main/OP%20OP%20Gun%20Script%20V0.1", true))()
+end)
+
+local b = w:CreateFolder("Pistol 1v1",{
+    TextSize = 25;
+    TextColor = Color3.fromRGB(255,255,255);
+    BgColor = Color3.fromRGB(0,0,0);
+})
+
+b:Button("Pistol 1v1", function()
+        loadstring(game:HttpGet("loadstring(game:HttpGet('https://raw.githubusercontent.com/zReal-King/1v1-Pistol-/main/Gui'))()"))()
+end)
+
+local b = w:CreateFolder("Placeholder",{
+    TextSize = 25;
+    TextColor = Color3.fromRGB(255,255,255);
+    BgColor = Color3.fromRGB(0,0,0);
+})
+
+b:Button("Placeholder", function()
+        loadstring(game:HttpGet(""))()
+end)
+
+local b = w:CreateFolder("Placeholder",{
+    TextSize = 25;
+    TextColor = Color3.fromRGB(255,255,255);
+    BgColor = Color3.fromRGB(0,0,0);
+})
+
+b:Button("Placeholder", function()
+        loadstring(game:HttpGet(""))()
+end)
+
+
+local b = w:CreateFolder("Placeholder",{
+    TextSize = 25;
+    TextColor = Color3.fromRGB(255,255,255);
+    BgColor = Color3.fromRGB(0,0,0);
+})
+
+b:Button("Placeholder", function()
+        loadstring(game:HttpGet(""))()
+end)
+
+
+local b = w:CreateFolder("Placeholder",{
+    TextSize = 25;
+    TextColor = Color3.fromRGB(255,255,255);
+    BgColor = Color3.fromRGB(0,0,0);
+})
+
+b:Button("Placeholder", function()
+        loadstring(game:HttpGet(""))()
+end)
+
+
+local b = w:CreateFolder("Placeholder",{
+    TextSize = 25;
+    TextColor = Color3.fromRGB(255,255,255);
+    BgColor = Color3.fromRGB(0,0,0);
+})
+
+b:Button("Placeholder", function()
+        loadstring(game:HttpGet(""))()
+end)
+
+game:GetService("StarterGui"):SetCore("SendNotification", {
+Title = "Script Hub";
+Text = "Executed, look in the top left.";
+Duration = 8;
+})
+
+game:GetService("StarterGui"):SetCore("SendNotification", {
+Title = "Hide Keybind";
+Text = "Press Right Ctrl to hide the gui.";
+Duration = 12.5;
+})
