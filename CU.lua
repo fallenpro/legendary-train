78df8dca6391d4864fd912e5299b0178c3a220f7
Tanki <100938045+fheahdythdr@users.noreply.github.com>
2022-03-05 18:08:56 +0200

    Update CU.lua

--- CU.lua
+++ CU.lua
@@ -1,9 +1,13 @@
 local library = loadstring(game:HttpGet(('https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/wall%20v3')))()
 local w = library:CreateWindow("Catter's Utilities")
-local b = w:CreateFolder("Aimhot, Infinite Store, etc.")
+local b = w:CreateFolder("GUI Utils")
 
-b:Button("AimHot", function()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Herrtt/AimHot-v8/master/Main.lua", true))()
+b:Button("BackdoorChecker", function()
+    loadstring(game:HttpGet(('https://raw.githubusercontent.com/iK4oS/backdoor.exe/master/source.lua'),true))()
+end)
+
+b:Button("Cripware", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/q0maHVH/cripware/main/main"))()
 end)
 
 b:Button("CMD-X", function()
@@ -22,7 +26,7 @@ b:Button("Server Browser", function()
         loadstring(game:HttpGet("https://raw.githubusercontent.com/Lynk-Softworks/Valerie/main/serverBrowser.lua", true))()
 end)
 
-local b = w:CreateFolder("Mostly Universal",{
+local b = w:CreateFolder("Hubs",{
     TextSize = 25;
     TextColor= Color3.fromRGB(255,255,255);
     BgColor = Color3.fromRGB(0,0,0);
@@ -37,6 +41,30 @@ b:Button("Cattori Hub", function()
     loadstring(game:HttpGet("https://cattori.xyz/main.lua"))()
 end)
 
+b:Button("Ghost Hub", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/ScriptRUs/Ryans/main/Ghosthub.lua"))()
+end)
+
+b:Button("Sus Hub", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/anogus/sushub/main/sushub.lua"))()
+end)
+
+b:Button("Arsenal Dex Hub", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/HonestlyDex/DexHub/main/Init"))()
+end)
+
+b:Button("Snow Hub RED", function()
+    loadstring(game:HttpGet("https://snowhub.dev/robloxscripts/Init", true))()
+end)
+
+b:Button("White Web Hub", function()
+    loadstring(game:GetObjects('rbxassetid://6392279388')[1].Source)() 
+end)
+
+b:Button("Arsenal Bolts Hub", function()
+    loadstring(game:HttpGet(("https://raw.githubusercontent.com/fusiongreg/BoltsHubV3/main/BoltsHubv3"),true))()
+end)
+
 local b = w:CreateFolder("Memes",{
     TextSize = 25;
     TextColor = Color3.fromRGB(255,255,255);
@@ -49,6 +77,20 @@ end)
 
 b:Button("Invis Car", function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/fheahdythdr/miniature-tribble/main/car.lua"))()
+end)
+
+local b = w:CreateFolder("Arsenal",{
+    TextSize = 25;
+    TextColor= Color3.fromRGB(255,255,255);
+    BgColor = Color3.fromRGB(0,0,0);
+})
+
+b:Button("KillAll", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/fheahdythdr/miniature-tribble/main/KillAll.lua"))()
+end)
+
+b:Button("Autofarm", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/fheahdythdr/miniature-tribble/main/Autofarm.lua"))()
 end)
 
 local b = w:CreateFolder("BIG Paintball",{
