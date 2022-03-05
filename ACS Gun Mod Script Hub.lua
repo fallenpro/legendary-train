b7bc74bf8c04044c7aca7e10b0c1c95798a10713
Tanki <100938045+fheahdythdr@users.noreply.github.com>
2022-03-05 11:31:50 +0200

    Update ACS Gun Mod Script Hub.lua

--- ACS Gun Mod Script Hub.lua
+++ ACS Gun Mod Script Hub.lua
@@ -367,3 +367,9 @@ Title = "Hide Keybind";
 Text = "Press Right Ctrl to hide the gui.";
 Duration = 12.5;
 })
+
+game:GetService("StarterGui"):SetCore("SendNotification", {
+Title = "Update.";
+Text = "Updated UI";
+Duration = 12.5;
+})
