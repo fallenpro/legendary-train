aef43824241c6785589418b242b369be69080407
Tanki <100938045+fheahdythdr@users.noreply.github.com>
2022-03-05 18:15:52 +0200

    Create BombSecondaryArsenal.lua

---
+++ BombSecondaryArsenal.lua
@@ -0,0 +1,5 @@
+getsenv(game.Players.LocalPlayer.PlayerGui.GUI.Client).fakeadmin=true
+debug.setconstant(getsenv(game.Players.LocalPlayer.PlayerGui.GUI.Client).givetools,40,"fakeadmin")
+debug.setconstant(getsenv(game.Players.LocalPlayer.PlayerGui.GUI.Client).givetools,43,game.Players.LocalPlayer.Name)
+debug.setconstant(getsenv(game.Players.LocalPlayer.PlayerGui.GUI.Client).givetools,45,"Bomb")
+getsenv(game.Players.LocalPlayer.PlayerGui.GUI.Client).givetools()
