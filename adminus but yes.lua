c19dbb2af8fb292f39575b648d243091b4e0ffe9
Tanki <100938045+fheahdythdr@users.noreply.github.com>
2022-03-19 14:19:25 +0200

    Create adminus but yes.lua

---
+++ adminus but yes.lua
@@ -0,0 +1,17 @@
+--[[ Supported Games:
+Prison Life ( Default )
+Ninja Legends ( Default , New )
+Super Power Training Simulator  ( Default )
+Clicker Simulator ( New )
+Giant Survival ( New ) 
+Tower of Hell ( New )
+Boom Simulator ( Default )
+]]
+
+local Theme = "Default" ---- change to New if u want a cool intro
+
+if Theme == "Default" then
+loadstring(game:HttpGet"https://rawscripts.net/raw/Universal-Script-ADMINUS-GUI-(UPDATE)-1290")()
+elseif Theme == "New" then
+loadstring(game:HttpGet"https://rawscripts.net/raw/Universal-Script-test-1232")()
+end
