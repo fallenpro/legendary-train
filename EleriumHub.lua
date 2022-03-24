5ac4aa12c528172913fd08742555db9b453f7f32
Tanki <100938045+fheahdythdr@users.noreply.github.com>
2022-03-24 20:06:09 +0200

    Update EleriumHub.lua

--- EleriumHub.lua
+++ EleriumHub.lua
@@ -1,3 +1,4 @@
+local repeattimes = repeatamount
 local message = bruh
 
 local ui_options = {
@@ -2429,7 +2430,35 @@ do -- Example UI
                 bypass()
             end
             
-        end)
+        	end)
+	
+		Tab:AddTextBox("Remote Event Multiplier", function(repeattimes)
+		
+		local settings = {repeatamount = 40, exceptions = {"SayMessageRequest","MeleeUpdateEvent","NinjaBombEvent","BulletUpdateEvent"}}
+
+		local mt = getrawmetatable(game)
+		local old = mt.__namecall
+		setreadonly(mt, false)
+
+		mt.__namecall = function(uh, ...)
+   			local args = {...}
+   			local method = getnamecallmethod()
+   			for i,o in next, settings.exceptions do
+       				if uh.Name == o then
+           				return old(uh, ...)
+       				end
+   			end
+   			if method == "FireServer" or method == "InvokeServer" then
+       				for i = 1,settings.repeatamount do
+           				old(uh, ...)
+       				end
+   			end
+   			return old(uh, ...)
+		end
+
+		setreadonly(mt, true)
+		
+		end)
 		
 		local Tab = Window:AddTab("Exuny")
 		
