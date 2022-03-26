c5009ecfa8270f82cf26b63efa6bdc8ac21784f7
Tanki <100938045+fheahdythdr@users.noreply.github.com>
2022-03-26 16:09:12 +0200

    Create acs gun mods.lua

---
+++ acs gun mods.lua
@@ -0,0 +1,39 @@
+local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/wave"))()
+local Main = library:Main()
+local Tab = Main:Tab("ACS")
+local Section = Tab:Section("Gun Mods")
+Section:Item(
+    "button",
+    "Infinite Ammo, No Recoil and Firerate",
+    function()
+        library.Warn("Re-equip weapon.")
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/fheahdythdr/miniature-tribble/main/INFAMMO.lua"))()
+    end
+)
+
+Section:Item(
+    "button",
+    "Explosive Bullets + everything before",
+    function()
+        library.Warn("Re-equip weapon.")
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/fheahdythdr/miniature-tribble/main/ACS%20Giant%20Explosions.lua"))()
+    end
+)
+
+Section:Item(
+    "button",
+    "Extremely Explosive Bullets",
+    function()
+        library.Warn("Re-equip weapon.")
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/fheahdythdr/miniature-tribble/main/ACS%20Tsar%20Bomba%20Bullets.lua"))()
+    end
+)
+
+Section:Item(
+    "button",
+    "Infinite Ammo but DAMAGE",
+    function()
+        library.Warn("Re-equip weapon."
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/fheahdythdr/miniature-tribble/main/INFAMMO%20%2B%20DAMAGE.lua"))()
+    end
+)
