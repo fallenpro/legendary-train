78c5da27c8b4a01ea62150b7471b34d9fb865a95
Tanki <100938045+fheahdythdr@users.noreply.github.com>
2022-03-26 16:19:08 +0200

    Update acs gun mods.lua

--- acs gun mods.lua
+++ acs gun mods.lua
@@ -1,39 +1,27 @@
-local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/wave"))()
-local Main = library:Main()
-local Tab = Main:Tab("ACS")
-local Section = Tab:Section("Gun Mods")
-Section:Item(
-    "button",
-    "Infinite Ammo",
-    function()
-        library.Warn("Re-equip weapon.")
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/fheahdythdr/miniature-tribble/main/INFAMMO.lua"))()
-    end
-)
+--Made by : https://v3rmillion.net/member.php?action=profile&uid=912407
 
-Section:Item(
-    "button",
-    "Explosive Bullets",
-    function()
-        library.Warn("Re-equip weapon.")
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/fheahdythdr/miniature-tribble/main/ACS%20Giant%20Explosions.lua"))()
-    end
-)
+local ui = loadstring(game:HttpGet('https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/LaziestBoys'))()
 
-Section:Item(
-    "button",
-    "Extremely Explosive Bullets",
-    function()
-        library.Warn("Re-equip weapon.")
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/fheahdythdr/miniature-tribble/main/ACS%20Tsar%20Bomba%20Bullets.lua"))()
-    end
-)
+local window = ui:AddWindow('Gun Mods')
 
-Section:Item(
-    "button",
-    "Infinite Ammo but DAMAGE",
-    function()
-        library.Warn("Re-equip weapon.")
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/fheahdythdr/miniature-tribble/main/INFAMMO%20%2B%20DAMAGE.lua"))()
-    end
-)
+window:Button('Infinite Ammo', function()
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/fheahdythdr/miniature-tribble/main/INFAMMO.lua"))()
+end)
+
+window:Button('Inf Ammo but DAMAGE', function()
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/fheahdythdr/miniature-tribble/main/INFAMMO%20%2B%20DAMAGE.lua"))()
+end)
+
+window:Button('Explosive Bullets', function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/fheahdythdr/miniature-tribble/main/ACS%20Giant%20Explosions.lua"))()
+end)
+
+window:Button('Really Explosive Bullets', function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/fheahdythdr/miniature-tribble/main/ACS%20Tsar%20Bomba%20Bullets.lua"))()
+end)
+
+game:GetService("StarterGui"):SetCore("SendNotification", {
+Title = "GUN MOD INFO";
+Text = "RE-EQUIP ANY GUN AFTER EXECUTION";
+Duration = 25;
+})
