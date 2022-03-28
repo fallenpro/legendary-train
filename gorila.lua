0896c24e7631d8805de4f25e9d97daa714173923
Tanki <100938045+fheahdythdr@users.noreply.github.com>
2022-03-28 18:25:03 +0200

    Create gorila.lua

---
+++ gorila.lua
@@ -0,0 +1,15 @@
+local ui = loadstring(game:HttpGet('https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/LaziestBoys'))()
+
+local window = ui:AddWindow('gorila')
+
+window:Button('gorila tag', function()
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/fheahdythdr/miniature-tribble/main/gorila%20tag.lua"))()
+end)
+
+window:Button('netbypass', function()
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/fheahdythdr/miniature-tribble/main/netless.lua"))()
+end)
+
+window:Button('r6 gorila', function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/fheahdythdr/miniature-tribble/main/vrscript.lua"))()
+end)
