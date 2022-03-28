e66b33173875ea07eddfc00cb28d160c5529b8b5
Tanki <100938045+fheahdythdr@users.noreply.github.com>
2022-03-28 17:38:28 +0200

    Create vrnetless.lua

---
+++ vrnetless.lua
@@ -0,0 +1,3 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/fheahdythdr/miniature-tribble/main/netless.lua"))()
+wait(4)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/fheahdythdr/miniature-tribble/main/vrscript.lua"))()
