e5689656d83fe71afbddd9cc77a3b1fcf1dc2a94
Tanki <100938045+fheahdythdr@users.noreply.github.com>
2022-04-06 14:58:18 +0200

    change ui name and link being shown

--- CATSH REMAKE 3.lua
+++ CATSH REMAKE 3.lua
@@ -1,9 +1,9 @@
 local Mercury = loadstring(game:HttpGet("https://raw.githubusercontent.com/deeeity/mercury-lib/master/src.lua"))()
 local GUI = Mercury:Create{
-    Name = "Methamphetamine Productions",
+    Name = "quantum supremacy",
     Size = UDim2.fromOffset(600, 400),
     Theme = Mercury.Themes.Dark,
-    Link = "https://github.com/deeeity/mercury-lib"
+    Link = "https://github.com/fheahdythdr/legendary-train/blob/main/README.md"
 }
 local Tab = GUI:Tab{
 	Name = "Scripts",
