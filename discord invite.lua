442f76a0a9e154d205e8744fc658bff93cf20dac
Tanki <100938045+fheahdythdr@users.noreply.github.com>
2022-04-06 14:32:14 +0200

    Discord Invite module file made.

---
+++ discord invite.lua
@@ -0,0 +1,3 @@
+local inviteModule = loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Utilities/main/Discord%20Inviter/Source.lua"))()
+
+inviteModule.Join("https://discord.gg/2dz3BRDWuJ")
