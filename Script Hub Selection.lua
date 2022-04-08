3bd03eb80caa5878bb581fdc73b91bf3cd80d7e2
Tanki <100938045+fheahdythdr@users.noreply.github.com>
2022-04-08 20:39:42 +0200

    Update Script Hub Selection.lua

--- Script Hub Selection.lua
+++ Script Hub Selection.lua
@@ -1,45 +1,5 @@
-local Mercury = loadstring(game:HttpGet("https://raw.githubusercontent.com/deeeity/mercury-lib/master/src.lua"))()
-
-local GUI = Mercury:Create{
-    Name = "Selection",
-    Size = UDim2.fromOffset(600, 400),
-    Theme = Mercury.Themes.Dark,
-    Link = "https://github.com/fheahdythdr/legendary-train/blob/main/README.md"
-}
-
-local Tab = GUI:Tab{
-    Icon = "rbxassetid://4498590361",
-    Name = "Selection"
-}
-
-Tab:Button{
-    Name = "Script Hub",
-    Callback = function()
-        Tab:Prompt{
-            Title = "Which script hub do you want?",
-            Text = "Pick between Syn X only or other executors.",
-            Buttons = {
-                Synapse = function()
-                loadstring(game:HttpGet("https://raw.githubusercontent.com/fheahdythdr/legendary-train/main/EleriumHub.lua"))()
-                end,
-                Others = function()
-                loadstring(game:HttpGet("https://raw.githubusercontent.com/fheahdythdr/legendary-train/main/catsh%20remake%20again.lua"))()
-                end,
-            }
-        }
-    end,
-}
 if syn then
-GUI:Notification{
-	Title = "EXIT",
-	Text = "Press the X after executing synapse script hub.",
-	Duration = 12.5,
-	Callback = function() end
-}
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/fheahdythdr/legendary-train/main/EleriumHub.lua"))()
+else
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/fheahdythdr/legendary-train/main/catsh%20remake%20again.lua"))()
 end
-
-GUI:Credit{
-	Name = "Cold",
-	Description = "Contributed to the original hub, and put some scripts in one of his github repositories.",
-	Discord = "coldlolololir#6969"
-}
