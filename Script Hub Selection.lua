4905f46e602d4d10e29b0c4788258402db98c99b
Tanki <100938045+fheahdythdr@users.noreply.github.com>
2022-04-29 20:40:28 +0200

    Update Script Hub Selection.lua

--- Script Hub Selection.lua
+++ Script Hub Selection.lua
@@ -1,5 +1,5 @@
 if syn then
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/fheahdythdr/legendary-train/main/EleriumHub.lua"))()
+	loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/fheahdythdr/legendary-train/main/EleriumHub.lua"))()
 else
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/fheahdythdr/legendary-train/main/catsh%20remake%20again.lua"))()
+	loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/fheahdythdr/legendary-train/main/catsh%20remake%20again.lua"))()
 end
