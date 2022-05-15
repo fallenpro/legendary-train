7a2cfb1ab4b0d17d4e1dca8c3c2402454258af7e
Tanki <100938045+fheahdythdr@users.noreply.github.com>
2022-05-15 14:16:00 +0200

    add dark dex v3

--- EleriumHub.lua
+++ EleriumHub.lua
@@ -2432,7 +2432,11 @@ do -- Example UI
 			loadstring(game:HttpGet("https://www.scriptblox.com/raw/Server-Browser_80",true))()
 		end)
 		
-		Tab:AddButton("Dark Dex", function()
+		Tab:AddButton("Dark Dex (V3)", function()
+			loadstring(game:HttpGet("https://raw.githubusercontent.com/fheahdythdr/legendary-train/main/dark%20dex%20v3.lua"))()
+		end)
+		
+		Tab:AddButton("Dark Dex (V4)", function()
 			loadstring(game:HttpGet("https://raw.githubusercontent.com/fheahdythdr/legendary-train/main/dark%20dex.lua"))()
 		end)
 		
