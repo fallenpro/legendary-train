558f6d57812eb64dd40928ab61cca243868642d4
Tanki <100938045+fheahdythdr@users.noreply.github.com>
2022-05-30 15:47:21 +0200

    Update Script Hub Selection.lua

--- Script Hub Selection.lua
+++ Script Hub Selection.lua
@@ -1,3 +1,11 @@
+local verifyx = loadstring(game:HttpGet("https://raw.githubusercontent.com/fheahdythdr/ui-libs-ui-lib-backups/main/modified%20for%20own%20use/VerifyX"))()
+local vwindow = verifyx.new("Checking executor..", 0.25)
+wait(1)
+vwindow:Update("Executor: "..identifyexecutor(), 0.5)
+wait(1)
+vwindow:Update("Getting script hub version..", 0.75)
+wait(1)
+vwindow:Complete()
 if syn then
 	loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/fheahdythdr/legendary-train/main/EleriumHub.lua"))()
 else
