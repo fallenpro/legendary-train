ea5cb9524f0551952a0aee83b4db8f310ab8476b
Tanki <100938045+fheahdythdr@users.noreply.github.com>
2022-06-01 13:02:06 +0200

    Update Script Hub Selection.lua

--- Script Hub Selection.lua
+++ Script Hub Selection.lua
@@ -1,3 +1,4 @@
+--disclaimer, the verifyx ui wasn't made by me, it was made by icuck
 local verifyx = loadstring(game:HttpGet("https://raw.githubusercontent.com/fheahdythdr/ui-libs-ui-lib-backups/main/modified%20for%20own%20use/VerifyX"))()
 local vwindow = verifyx.new("Checking executor..", 0.25)
 wait(1)
