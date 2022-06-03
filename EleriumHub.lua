3d3b5248d54c95aff0d6833193bb4d411537ca3b
Tanki <100938045+fheahdythdr@users.noreply.github.com>
2022-06-03 22:20:39 +0200

    Update EleriumHub.lua

--- EleriumHub.lua
+++ EleriumHub.lua
@@ -1,5 +1,6 @@
 --[[
 	github slowwwwwwwwww
+	why it so slow
 ]]
 loadstring(game:HttpGet("https://raw.githubusercontent.com/fheahdythdr/legendary-train/main/antichatban.lua"))()
 local ESP = loadstring(game:HttpGet("https://raw.githubusercontent.com/Kiriot22/ESP-Lib/main/ESP.lua"))()
