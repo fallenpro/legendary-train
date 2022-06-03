365c53d5462f8d46dc3080972e8e888e044ecf48
Tanki <100938045+fheahdythdr@users.noreply.github.com>
2022-06-03 17:57:12 +0200

    add infinite yield CLI

--- EleriumHub.lua
+++ EleriumHub.lua
@@ -2474,6 +2474,29 @@ do -- Example UI
 		Tab:AddButton("Infinite Yield", function()
 			loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Infinite-Store/Infinite-Store/main/main.lua"))()
 		end)
+		Tab:AddLabel("CLI is Command Line, if you didn't know")
+		Tab:AddButton("Infinite Yield CLI (don't execute with normal iy)", function()
+			loadstring(game:HttpGet("https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source"), "IY_FE") "discord.gg/dYHag43eeU"
+			local function cls()
+			    rconsoleclear()
+			    rconsoleprint("@@WHITE@@")
+			    rconsolename("Infinite Yield")
+			    rconsoleprint("\nInfinite Yield\n\nType in the command and click enter. (No prefix needed)\n\n")
+			end
+			coroutine.wrap(function(Hunter, on, top)
+			    while true do
+			        rconsoleprint(">")
+			        local str = rconsoleinput()
+			        if str:lower() == "cls" then
+			            cls()
+			        else
+			            coroutine.wrap(xpcall)(execCmd, function(msg)
+			                rconsolewarn(debug.traceback("Running the '" .. str .. "' command caused an error:\n" .. msg), rconsoleprint("\n"))
+			            end, str)
+			        end
+			    end
+			end)(cls(), Hunter, was, here)
+		end)
 		
 		Tab:AddButton("Fate's Admin", function()
 			loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/fatesc/fates-admin/main/main.lua"))()
