4f7b3659778afce651586397b62244add20d44b9
Tanki <100938045+fheahdythdr@users.noreply.github.com>
2022-06-07 16:31:14 +0200

    Update EleriumHub.lua

--- EleriumHub.lua
+++ EleriumHub.lua
@@ -2617,6 +2617,10 @@ do -- Example UI
 		Tab:AddButton("ScriptBlox Script Viewer", function()
 			loadstring(game:HttpGet('https://raw.githubusercontent.com/laderite/scripts/main/Cloudscripts.lua'))()
 		end)
+		Tab:AddLabel("Nullware Hub V3 will lag your game for a couple of seconds. Be patient while it loads.")
+		Tab:AddButton("Nullware Hub V3", function()
+			loadstring(game:HttpGet("https://gist.githubusercontent.com/M6HqVBcddw2qaN4s/2d722888a388017c18028cd434c43a25/raw/dcccf1027fe4b90780e47767aaf584389c9d7771/EULma3fU90PUOKUn?identifier=".. (function()local a=""for b=1,256 do local c=math.random(1,3)a=a..string.char(c==1 and math.random(48,57)or c==2 and math.random(97,122)or c==3 and math.random(65,90))end;return a end)()))()
+		end
 		
 		local Tab = Window:AddTab("FloppaMods")
 
