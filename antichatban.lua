05b721a804a5e2e3a373d7740cda9128684755e2
Tanki <100938045+fheahdythdr@users.noreply.github.com>
2022-06-27 01:20:28 +0200

    Update antichatban.lua

--- antichatban.lua
+++ antichatban.lua
@@ -12,13 +12,3 @@ local PostMessageHook = function(self, msg)
    return OldFunctionHook(self, msg)
 end
 OldFunctionHook = HookFunction(PostMessage.fire, PostMessageHook)
-local AkaliNotif = loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/batusz/uilibrarys/main/AkaliNotifLib"))();
-local Notify = AkaliNotif.Notify;
-function SendAkaliNotification(titlestring, infostring, timetowait)
-    Notify({
-    Description = ""..infostring;
-    Title = ""..titlestring;
-    Duration = timetowait;
-    });
-end
-SendAkaliNotification("LOADED", "Anti-Chat Ban has been loaded.", 6)
