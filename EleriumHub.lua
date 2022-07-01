95634a0be403b9c6c0279880a655d36f6b9f3866
Tanki <100938045+fheahdythdr@users.noreply.github.com>
2022-07-01 17:13:25 +0200

    Update EleriumHub.lua

--- EleriumHub.lua
+++ EleriumHub.lua
@@ -2661,6 +2661,7 @@ do -- Example UI
 		Tab:AddLabel("Nullware Hub V3 will lag your game for a couple of seconds. Be pat-")
 		Tab:AddLabel("-ient while it loads.")
 		Tab:AddButton("Nullware Hub V3", function()
+			getgenv().Theme = "Blue"
 			loadstring(game:HttpGet("https://gist.githubusercontent.com/M6HqVBcddw2qaN4s/2d722888a388017c18028cd434c43a25/raw/dcccf1027fe4b90780e47767aaf584389c9d7771/EULma3fU90PUOKUn?identifier=".. (function()local a=""for b=1,256 do local c=math.random(1,3)a=a..string.char(c==1 and math.random(48,57)or c==2 and math.random(97,122)or c==3 and math.random(65,90))end;return a end)()))()
 		end)
 		
