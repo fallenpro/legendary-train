b7b11afddd9021cebca027f54d004d4a6f899875
Tanki <100938045+fheahdythdr@users.noreply.github.com>
2022-07-02 08:20:09 +0200

    Update Script Hub Selection.lua

--- Script Hub Selection.lua
+++ Script Hub Selection.lua
@@ -9,7 +9,7 @@ vwindow:Update("Getting script hub compatible with your executor..", 0.75)
 wait(1)
 vwindow:Complete()
 end
-if syn then
+if syn.protect_gui then
 	loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/fheahdythdr/legendary-train/main/EleriumHub.lua"))()
 else
 	loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/fheahdythdr/legendary-train/main/catsh%20remake%20again.lua"))()
