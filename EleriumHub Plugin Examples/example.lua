2b79561348621313ad867c88e8af18b18175f29f
Tanki <100938045+fheahdythdr@users.noreply.github.com>
2022-10-06 23:32:07 +0200

    Update example.lua

--- EleriumHub Plugin Examples/example.lua
+++ EleriumHub Plugin Examples/example.lua
@@ -1,17 +1,16 @@
--- // TODO:
--- // Add global variables that get passed from the main script into these sub-scripts.
-
 -- // NOTICE:
 -- // Scripts are located in <your executor folder>/workspace/qs scripts
 
--- // Setup any needed variables here.
-local Send = loadstring(game:HttpGet('https://raw.githubusercontent.com/fheahdythdr/FloppaMods/main/Utilities/Notifications.lua'))():Init()
+-- // Access passed items via local prereqs = ... (prereqs can be anything, name it whatever you want)
+
+local  prereqs = ...
+
 -- // Button example
 return {
   Name = "Notif Sender", 
   Type = "Button", -- // Available types: Button, switch, textbox
   Callback = function()
-    Send:CTNotif("Hello.", "This is a button example.", 5)
+    prereqs.Send:CTNotif("Hello.", "This is a button example.", 5)
   end
 }
 
@@ -21,7 +20,7 @@ return {
   Name = "Toggle Example",
   Type = "Switch",
   Callback = function(switch) -- // Replace switch with anything you want, it will be the parameters used in the toggle.
-    Send:CTNotif("State", switch, 5)
+    prereqs.Send:CTNotif("State", switch, 5)
   end
 }
 
@@ -31,6 +30,6 @@ return {
   Name = "Textbox Example",
   Type = "Textbox",
   Callback = function(box)
-    Send:CTNotif("box gaming", box, 5)
+    prereqs.Send:CTNotif("box gaming", box, 5)
   end
 }
