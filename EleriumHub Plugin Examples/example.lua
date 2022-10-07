76d817331a4833af468ce53d75f6649f7dda5e67
Tanki <100938045+fheahdythdr@users.noreply.github.com>
2022-10-07 03:22:40 +0200

    Update example.lua

--- EleriumHub Plugin Examples/example.lua
+++ EleriumHub Plugin Examples/example.lua
@@ -3,14 +3,15 @@
 
 -- // Access passed items via local prereqs = ... (prereqs can be anything, name it whatever you want)
 
-local  prereqs = ...
+local prereqs = ...
+local utils = prereqs.utils -- // Prereqs is seperated into two tables, prereqs.utils (built in) and prereqs.custom (utils added in by libraries)
 
 -- // Button example
 return {
   Name = "Notif Sender", 
   Type = "Button", -- // Available types: Button, switch, textbox
   Callback = function()
-    prereqs.Send:CTNotif("Hello.", "This is a button example.", 5)
+    utils.Send:CTNotif("Hello.", "This is a button example.", 5)
   end
 }
 
@@ -20,7 +21,7 @@ return {
   Name = "Toggle Example",
   Type = "Switch",
   Callback = function(switch) -- // Replace switch with anything you want, it will be the parameters used in the toggle.
-    prereqs.Send:CTNotif("State", switch, 5)
+    utils.Send:CTNotif("State", switch, 5)
   end
 }
 
@@ -30,6 +31,6 @@ return {
   Name = "Textbox Example",
   Type = "Textbox",
   Callback = function(box)
-    prereqs.Send:CTNotif("box gaming", box, 5)
+    utils.Send:CTNotif("box gaming", box, 5)
   end
 }
